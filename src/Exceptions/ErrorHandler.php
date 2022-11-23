fefd7fd556b8cc427094876866ca50fac7a023ae
sean <sxfmao@163.com>
2022-11-23 17:12:41 +0800

    commit

---
+++ src/Exceptions/ErrorHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rainte\Yii\Exceptions;
+
+use Rainte\Yii\Exceptions\Exception;
+use yii\web\Response;
+
+class ErrorHandler extends \yii\web\ErrorHandler
+{
+    /**
+     * @inheritdoc
+     */
+    protected function convertExceptionToArray($exception)
+    {
+        $res = parent::convertExceptionToArray($exception);
+
+        if (!YII_DEBUG && !$exception instanceof Exception) {
+            $res['name'] = 'Exception';
+            $res['code'] = 500;
+            $res['message'] = Response::$httpStatuses[500];
+        }
+
+        return $res;
+    }
+}
